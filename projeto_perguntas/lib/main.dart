02a29c56357eca7d6f5da637f59d1bfefae0eccb
AlexandreFel1pe <alexandre.borges900@gmail.com>
2025-11-05 15:39:43 -0300

    Add ElevatedButton

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -3,14 +3,41 @@ import 'package:flutter/material.dart';
 main() => runApp(PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
+  void responder() {
+    print('Pergunta respondida.');
+  }
+
   @override
   Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?'
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Text('Olá Flutter!!!'),
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            ElevatedButton(
+              onPressed: responder,
+              child: Text('Resposta 1'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print('Resposta 2 foi selecionada.');
+              },
+              child: Text('Resposta 2'),
+            ),
+            ElevatedButton(
+              onPressed: () => print('Resposta 3!!!'),
+              child: Text('Resposta 3'),
+            ),
+          ],
+        ),
       ),
     );
   }
