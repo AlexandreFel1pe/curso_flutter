05e8376e6ee7c75c885714144f5dec0acbcb092e
AlexandreFel1pe <alexandre.borges900@gmail.com>
2025-11-06 10:06:47 -0300

    add widget list imperative

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -40,6 +40,12 @@ class _PerguntaAppState extends State<PerguntaApp> {
       },
     ];
 
+    List<Widget> respostas = [];
+
+    for (var textoResp in perguntas[_perguntaSelecionada]['respostas'] as List<String>) {
+      respostas.add(Resposta(textoResp, _responder));
+    }
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -48,9 +54,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
         body: Column(
           children: [
             Questao('${perguntas[_perguntaSelecionada]['texto']}'),
-            Resposta('Resposta 1', _responder),
-            Resposta('Resposta 2', _responder),
-            Resposta('Resposta 3', _responder),
+            ...respostas,
           ],
         ),
       ),
