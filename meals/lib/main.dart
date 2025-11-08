3ff371cde5b0a43ed5cb2cbc4d952387112fb9d4
AlexandreFel1pe <alexandre.borges900@gmail.com>
2025-11-08 12:35:27 -0300

    big update. add categories and category item screens, change style, navigate to screen, passing data with constructors, named routes

--- meals/lib/main.dart
+++ meals/lib/main.dart
@@ -1,35 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:meals/screens/categories_meals_screen.dart';
+import 'package:meals/screens/categories_screen.dart';
+import 'package:meals/utils/app_routes.dart';
  
 void main() => runApp(MyApp());
  
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        fontFamily: 'Raleway',
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        textTheme: ThemeData.light().textTheme.copyWith(
+          titleMedium: TextStyle(
+            fontFamily: 'RobotoCondensed'
+          ),
+        ),
       ),
-      home: MyHomePage(),
-    );
-  }
-}
- 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
- 
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('DeliMeals'),
-      ),
-      body: Center(
-        child: Text('Navegar é preciso!!'),
-      ),
+      routes: {
+        AppRoutes.HOME: (_) => CategoriesScreen(),
+        AppRoutes.CATEGORIES_MEALS: (_) => CategoriesMealsScreen(),
+      },
     );
   }
 }
