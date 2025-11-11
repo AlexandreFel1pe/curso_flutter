3d54097bb33f067620fa42eea1ce9d5e781e4bea
AlexandreFel1pe <alexandre.borges900@gmail.com>
2025-11-11 16:41:20 -0300

    add grid widget

--- shoop/lib/views/products_overview_screen.dart
+++ shoop/lib/views/products_overview_screen.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:shop/data/dummy_data.dart';
+import 'package:shop/models/product.dart';
 
 class ProductsOverviewScreen extends StatelessWidget {
-  const ProductsOverviewScreen({super.key});
+  ProductsOverviewScreen({super.key});
+
+  final List<Product> loadedProducts = DUMMY_PRODUCTS;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Minha Loja'),
+      ),
+      body: GridView.builder(
+        padding: EdgeInsets.all(10),
+        itemCount: loadedProducts.length,
+        itemBuilder: (ctx, index) => Text(loadedProducts[index].title),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+    ));
   }
 }
