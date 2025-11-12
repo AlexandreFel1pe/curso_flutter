2b942d60924a0c48f7a8f5c38a5f60a2ecace7c6
AlexandreFel1pe <alexandre.borges900@gmail.com>
2025-11-12 14:12:46 -0300

    mixin study

--- shoop/lib/views/product_detail_screen.dart
+++ shoop/lib/views/product_detail_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:shop/models/product.dart';
-import 'package:shop/providers/counter_provider.dart';
 
 class ProductDetailScreen extends StatefulWidget {
   const ProductDetailScreen({super.key});
@@ -16,27 +15,6 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text(product.title),
-      ),
-      body: Column(
-        children: [
-          Text(CounterProvider.of(context).state.value.toString()),
-          ElevatedButton(
-            onPressed: () {
-              setState(() {
-                CounterProvider.of(context).state.inc();
-                print(CounterProvider.of(context).state.value);
-              });
-
-              // Forma diferente que faz a mesma coisa //
-              /*
-                setState(() {
-                  context
-                    .dependOnInheritedWidgetOfExactType<CounterProvider>()?.state.inc();
-                });
-              */
-            },
-            child: Text('+')),
-        ],
       ),
     );
   }
