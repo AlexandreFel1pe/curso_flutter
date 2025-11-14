f3a0af03c2ce9f7ab70fdae842c3ad77501f852c
AlexandreFel1pe <alexandre.borges900@gmail.com>
2025-11-14 14:22:19 -0300

    add cart widget

---
+++ shoop/lib/views/cart_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/providers/cart.dart';
+
+class CartScreen extends StatelessWidget {
+  const CartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final Cart cart = Provider.of(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Carrinho'),
+      ),
+      body: Column(
+        children: [
+          Card(
+            elevation: 0,
+            margin: EdgeInsets.all(25),
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Total',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Chip(
+                    label: Text(
+                      'R\$ ${cart.totalAmount.toStringAsFixed(2)}',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    backgroundColor: Theme.of(context).primaryColor,
+                  ),
+                  Spacer(),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text('COMPRAR'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- shoop/lib/views/products_overview_screen.dart
+++ shoop/lib/views/products_overview_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/providers/cart.dart';
+import 'package:shop/utils/app_routes.dart';
 import 'package:shop/widgets/badge.dart';
 import 'package:shop/widgets/product_grid.dart';
 
@@ -55,7 +56,9 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
               value: cart.itemCount.toString(),
               color: Colors.red,
               child: IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.of(context).pushNamed(AppRoutes.CART);
+                },
                 icon: Icon(Icons.shopping_cart),
               ),
             ),
