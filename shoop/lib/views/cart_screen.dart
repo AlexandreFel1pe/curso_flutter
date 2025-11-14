7f1809bab0da997bb94762533dca4f41b676f7e0
AlexandreFel1pe <alexandre.borges900@gmail.com>
2025-11-14 16:27:21 -0300

    add order provide and add orders

--- shoop/lib/views/cart_screen.dart
+++ shoop/lib/views/cart_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/providers/cart.dart';
+import 'package:shop/providers/orders.dart';
 import 'package:shop/widgets/cart_item_widget.dart';
 
 class CartScreen extends StatelessWidget {
@@ -43,7 +44,9 @@ class CartScreen extends StatelessWidget {
                   ),
                   Spacer(),
                   TextButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Provider.of<Orders>(context, listen: false).addorder(cart);
+                    },
                     child: Text('COMPRAR'),
                   ),
                 ],
